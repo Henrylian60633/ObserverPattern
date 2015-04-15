e252f840c0860817cdf48841cb884358ae92a882
Henrylian60633 <h569696@gmail.com>
2015-04-15 15:47:50 +0800

    OberserverPattern

---
+++ src/Observer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 子恒
+ */
+public abstract class Observer {
+   protected Subject subject;
+   public abstract void update();
+}
